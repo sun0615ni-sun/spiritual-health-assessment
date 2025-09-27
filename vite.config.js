24c16cf8edcea753d84969e6d4f49ecf07313e19
sun0615ni-sun <sun0615ni@gmail.com>
2025-09-27 18:55:27 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  // IMPORTANT: 將 base 設為你的 repo 名稱路徑（注意前後斜線）
   base: '/spiritual-health-assessment/',
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
-  },
-});
+  build: {
+    outDir: 'dist'
+  }
+})
