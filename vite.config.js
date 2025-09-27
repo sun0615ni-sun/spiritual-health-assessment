b27aab9238c61a2aba67d32fd6ad454bfb03100b
sun0615ni-sun <sun0615ni@gmail.com>
2025-09-27 09:13:00 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
-  // *** 最重要：設定 GitHub Pages 的 base 為 repo 名稱（注意前後斜線） ***
+  // IMPORTANT: 將 base 設為你的 repo 名稱路徑（注意前後斜線）
   base: '/spiritual-health-assessment/',
   plugins: [react()],
   resolve: {
